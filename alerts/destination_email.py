a2e760c18c09cf8475d1b2b17a3a47b2f4a9ea03
Iaroslav Shepilov <buggygm@gmail.com>
2017-02-21 08:18:43 +0100

    #54 added possibility to disable auth and tls for email notifications

--- alerts/destination_email.py
+++ alerts/destination_email.py
@@ -18,12 +18,18 @@ class EmailDestination(destination_base.Destination):
     from_address = None
     to_addresses = None
     server = None
+    login = None
     password = None
+    auth_enabled = None
+    tls = None
 
     def __init__(self, params_dict):
         self.from_address = params_dict.get('from')
         self.to_addresses = params_dict.get('to')
         self.server = params_dict.get('server')
+        self.auth_enabled = params_dict.get('auth_enabled')
+        self.login = params_dict.get('login')
+        self.tls = params_dict.get('tls')
 
         self.password = self.read_password(params_dict)
         self.to_addresses = split_addresses(self.to_addresses)
@@ -37,16 +43,29 @@ class EmailDestination(destination_base.Destination):
         if not self.server:
             raise Exception('"server" is compulsory parameter for email destination')
 
-        if not self.password:
-            raise Exception('"password" is compulsory parameter for email destination')
+        if self.auth_enabled is None:
+            auth_enabled = self.password or self.login
+        elif (self.auth_enabled == True) or (self.auth_enabled.lower() == 'true'):
+            self.auth_enabled = True
+            if not self.login:
+                self.login = self.from_address
+        else:
+            self.auth_enabled = False
+
+        if (self.tls is None) and ('gmail' in self.server):
+            self.tls = True
 
     @staticmethod
     def read_password(params_dict):
         password = params_dict.get('password')
         if password and password.startswith('$$'):
-            env_password = os.environ[password[2:]]
+            variable_name = password[2:]
+            env_password = os.environ[variable_name]
             if env_password:
                 password = env_password
+            elif env_password is None:
+                raise Exception('Password environment variable ' + variable_name + ' is not set')
+
         return password
 
     def send(self, title, body):
@@ -54,10 +73,15 @@ class EmailDestination(destination_base.Destination):
 
         server = smtplib.SMTP(self.server)
         server.ehlo()
-        server.starttls()
-        server.login(self.from_address, self.password)
+
+        if self.tls:
+            server.starttls()
+
+        if self.auth_enabled:
+            server.login(self.login, self.password)
+
         server.sendmail(self.from_address, self.to_addresses, message)
         server.quit()
 
     def __str__(self, *args, **kwargs):
-        return 'mail to ' + str(self.to_addresses) + ' over ' + self.from_address
+        return 'mail to ' + '; '.join(self.to_addresses) + ' over ' + self.from_address
