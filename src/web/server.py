ed4378db95b30bb90402b263e227739ba9aca586
bugy <buggygm@gmail.com>
2018-12-27 23:42:23 +0100

    fixed not loading static data for login page

--- src/web/server.py
+++ src/web/server.py
@@ -51,15 +51,15 @@ def is_allowed_during_login(request_path, login_url, request_handler):
     if request_path == login_url:
         return True
 
-    login_resources = ['/js/login.js',
-                       '/js/common.js',
-                       '/js/libs/jquery.min.js',
-                       '/js/libs/materialize.js',
-                       '/css/libs/materialize.min.css',
+    login_resources = ['/login.js',
+                       '/login.js.map',
                        '/css/index.css',
-                       '/css/fonts/roboto/Roboto-Regular.woff2',
-                       '/css/fonts/roboto/Roboto-Regular.woff',
-                       '/css/fonts/roboto/Roboto-Regular.ttf',
+                       '/login-deps.css',
+                       '/login-deps.css.map',
+                       '/fonts/roboto-latin-700.woff2',
+                       '/fonts/roboto-latin-700.woff',
+                       '/fonts/roboto-latin-400.woff2',
+                       '/fonts/roboto-latin-400.woff',
                        '/images/titleBackground.jpg',
                        '/images/g-logo-plain.png',
                        '/images/g-logo-plain-pressed.png']
@@ -73,7 +73,7 @@ def is_allowed_during_login(request_path, login_url, request_handler):
     else:
         return False
 
-    allowed_referrers = [login_url, '/css/libs/materialize.min.css', '/css/index.css']
+    allowed_referrers = [login_url, '/login-deps.css', '/css/index.css']
     for allowed_referrer in allowed_referrers:
         if referer.endswith(allowed_referrer):
             return True
