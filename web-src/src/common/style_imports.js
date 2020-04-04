5b09a45afcd0bfda2728e3d957a3d137ed3c115f
yshepilov <buggygm@gmail.com>
2020-04-04 22:13:53 +0200

    #282 fixed wrong css priorities due to file order issues

--- web-src/src/common/style_imports.js
+++ web-src/src/common/style_imports.js
@@ -2,9 +2,10 @@ import 'material-design-icons/iconfont/material-icons.css';
 import 'materialize-css/js/cash';
 import 'materialize-css/js/global';
 import 'materialize-css/js/waves'
-import 'materialize-css/sass/components/_buttons.scss';
+import 'typeface-roboto';
 
+// DO NOT TOUCH ORDER
 import 'materialize-css/sass/components/_color-classes.scss';
 import 'materialize-css/sass/components/_grid.scss';
+import 'materialize-css/sass/components/_buttons.scss';
 import 'materialize-css/sass/components/_waves.scss';
-import 'typeface-roboto';
--- web-src/src/main-app/MainApp.vue
+++ web-src/src/main-app/MainApp.vue
@@ -59,4 +59,7 @@
 </script>
 
 <style scoped>
+    h1, h2, h3, h4, h5, h6 {
+        margin: 0;
+    }
 </style>
