1d345608b7ee5902c98a0998f4b8c52cb0d23208
yshepilov <buggygm@gmail.com>
2020-04-08 22:48:01 +0200

    #282 removed obsolete webpack files

--- web-src/webpack.mutation.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-    entry: {'test_entry': './tests/terminal_model_test.js'},
-    mode: 'development',
-    devtool: 'inline-source-map'
-});
