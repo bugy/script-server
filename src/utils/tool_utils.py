83614a7d0586558cd3d867f041f9e8f3e58cece9
yshepilov <buggygm@gmail.com>
2020-04-16 21:46:17 +0200

    #282 updated build process according to new vue-cli structure

--- src/utils/tool_utils.py
+++ src/utils/tool_utils.py
@@ -14,7 +14,7 @@ def validate_web_build_exists(project_path):
     if not os.path.exists(web_folder):
         raise InvalidWebBuildException(web_folder + ' does not exist. \n' + how_to_fix_build_message)
 
-    required_files = ['index.js', 'index.html', 'index-deps.css', 'admin.html', 'admin.js']
+    required_files = ['index.html', 'admin.html', 'login.html', 'js', 'css', 'img']
     for file in required_files:
         file_path = os.path.join(web_folder, file)
 
