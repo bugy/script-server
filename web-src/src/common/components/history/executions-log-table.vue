995fec13b1ad8409407e98d804bfef7b974513b0
yshepilov <buggygm@gmail.com>
2020-05-23 13:45:52 +0200

    #145 added ID column to script history (to easier find executed scripts)

--- web-src/src/common/components/history/executions-log-table.vue
+++ web-src/src/common/components/history/executions-log-table.vue
@@ -3,6 +3,7 @@
         <table class="highlight striped">
             <thead>
             <tr>
+                <th class="id-column">ID</th>
                 <th class="start_time-column">Start Time</th>
                 <th class="user-column">User</th>
                 <th class="script-column">Script</th>
@@ -11,6 +12,7 @@
             </thead>
             <tbody v-if="!loading">
             <tr :key="row.id" @click="rowClick(row)" v-for="row in rows">
+                <td>{{ row.id }}</td>
                 <td>{{ row.startTimeString }}</td>
                 <td>{{ row.user }}</td>
                 <td>{{ row.script }}</td>
@@ -44,6 +46,10 @@
         cursor: pointer;
     }
 
+    .executions-log-table .id-column {
+        width: 10%;
+    }
+
     .executions-log-table .start_time-column {
         width: 25%;
     }
@@ -53,7 +59,7 @@
     }
 
     .executions-log-table .script-column {
-        width: 35%;
+        width: 25%;
     }
 
     .executions-log-table .status-column {
