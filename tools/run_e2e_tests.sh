88fe554c78276450b3bb453e14b187f90dd6772e
yshepilov <buggygm@gmail.com>
2021-04-20 23:51:42 +0200

    #431 added allure reporting

--- tools/run_e2e_tests.sh
+++ tools/run_e2e_tests.sh
@@ -5,17 +5,19 @@ set -e
 mkdir -p conf/runners
 cp samples/configs/* conf/runners/
 
-./launcher.py &
+./launcher.py > /dev/null &
 SERVER_PID=$!
 
 set +e
 
 cd src/e2e_tests
 
-../../e2e_venv/bin/python -m pytest
+../../e2e_venv/bin/python -m pytest --alluredir /tmp/allure_result
 STATUS=$?
 
 kill $SERVER_PID
 rm -rf conf/runners
+
+allure generate /tmp/allure_result --clean -o /tmp/allure_report
 
 exit $STATUS
