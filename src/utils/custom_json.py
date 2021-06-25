bed3cdf8f2633767011913aa6c739e1c0107773e
Bruno Almeida <balmeida@amelco.co.uk>
2021-06-25 14:28:03 +0100

    without this migration is broken

--- src/utils/custom_json.py
+++ src/utils/custom_json.py
@@ -1,9 +1,9 @@
 import json
 import re
 
-def loads (content):
+def loads (content, **args):
     contents=''
     for line in content.split('\n'):
         if not re.match (r'\s*//.*', line):
             contents += line + "\n"
-    return json.loads(contents)
+    return json.loads(contents, **args)
