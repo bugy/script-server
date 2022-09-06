8775db991a6e3b507069015c35f8bef81a5c98ee
Anthony FRANCISCO <anthony.francisco@novadys.fr>
2022-09-06 10:14:45 +0000

    Improve pull request about YAML support

--- src/utils/custom_yaml.py
+++ src/utils/custom_yaml.py
@@ -1,10 +1,5 @@
-import json
 import re
 import yaml
 
 def loads (content, **args):
-    contents=''
-    for line in content.split('\n'):
-        if not re.match (r'\s*//.*', line):
-            contents += line + "\n"
-    return yaml.safe_load(contents, **args)
+    return yaml.safe_load(content, **args)
