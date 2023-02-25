035b3e643edcbd0f577e16b33968e6a741d3409d
yshepilov <buggygm@gmail.com>
2023-02-25 22:14:15 +0100

    fixed missing auth for github push

--- tools/add_git_tag.sh
+++ tools/add_git_tag.sh
@@ -7,10 +7,12 @@ if [ -z "$TRAVIS_BRANCH" ] || [ -z "$OWNER" ] || [ -z "$GITHUB_TOKEN" ] || [ -z 
     exit -1
 fi
 
+git remote add gh https://${OWNER}:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git;
+
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     export NEW_GIT_TAG='dev'
-    GIT_TRACE=1 git tag -d 'dev'
-    GIT_TRACE=1 git push --delete origin 'dev'
+    git tag -d 'dev'
+    git push --delete gh 'dev'
 
 elif [ "$TRAVIS_BRANCH" == "stable" ]; then
     version=`unzip -qc build/script-server.zip version.txt`
@@ -26,7 +28,6 @@ else
     git config --local user.name 'bugy';
     git config --local user.email 'buggygm@gmail.com';
     git tag -f "$NEW_GIT_TAG";
-    git remote add gh https://${OWNER}:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git;
     git push -f gh "$NEW_GIT_TAG";
     git remote remove gh;
 fi
