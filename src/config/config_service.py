7d116d70f77128d3f6cf66c3db1821c7bd2f7edb
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-04-02 15:52:45 +0200

    use dot as seperator

--- src/config/config_service.py
+++ src/config/config_service.py
@@ -52,7 +52,7 @@ def _preprocess_incoming_config(config):
 def _create_archive_filename(filename):
     current_datetime = datetime.now()
     formatted_datetime = current_datetime.strftime('%Y%m%d%H%M%S')
-    return f"{formatted_datetime}_{filename}.deleted"
+    return f"{formatted.datetime}_{filename}.deleted"
 
 
 class ConfigService:
