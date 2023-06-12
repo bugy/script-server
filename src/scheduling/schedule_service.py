5ada7fdb8edeaa597d14a499aea40b30edf0dbf9
UrekD <urek.denis+git@gmail.com>
2023-06-12 20:30:52 +0200

    Improve scheduling logic for count

--- src/scheduling/schedule_service.py
+++ src/scheduling/schedule_service.py
@@ -123,13 +123,17 @@ class ScheduleService:
         if not schedule.repeatable and date_utils.is_past(schedule.start_datetime):
             return
         
-        if schedule.endOption == 'after' and schedule.endArg <= 0:
-            return
+        if schedule.endOption == 'after':
+            if schedule.endArg > 1:
+                schedule.endArg -= 1
+            else:
+                return
         
         next_datetime = schedule.get_next_time()
 
-        if schedule.endOption == 'on' and date_utils.is_past(schedule.endArg) or next_datetime < schedule.endArg:
-            return
+        if schedule.endOption == 'on':
+            if date_utils.is_past(schedule.endArg) or next_datetime < schedule.endArg:
+                return
         
         LOGGER.info(
             'Scheduling ' + job.get_log_name() + ' at ' + next_datetime.astimezone(tz=None).strftime('%H:%M, %d %B %Y'))
