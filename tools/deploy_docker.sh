fe016ec2f65cc9e102005995dd1effede8302510
Van Nghia <vanvnghia@gmail.com>
2023-11-07 20:28:55 +0100

    create builder and use push

--- tools/deploy_docker.sh
+++ tools/deploy_docker.sh
@@ -23,11 +23,11 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 
-docker buildx build --platform linux/amd64,linux/arm64 -f tools/Dockerfile -t "$IMAGE_NAME":"$DOCKER_TAG" .
+docker buildx create --use
+docker buildx build --platform linux/amd64,linux/arm64 --push -f tools/Dockerfile -t "$IMAGE_NAME":"$DOCKER_TAG" .
 
 echo "NEW_GIT_TAG=$NEW_GIT_TAG"
 if [ ! -z "$NEW_GIT_TAG" ]; then
   docker tag "$IMAGE_NAME":"$DOCKER_TAG" "$IMAGE_NAME":"$NEW_GIT_TAG"
+  docker push "$IMAGE_NAME":"$NEW_GIT_TAG"
 fi
-
-docker push --all-tags "$IMAGE_NAME"
