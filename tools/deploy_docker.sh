f614278860d116b2d9148678e8998d84689ce47b
Iaroslav Shepilov <buggygm@gmail.com>
2023-11-07 13:20:25 +0100

    Update deploy_docker.sh

--- tools/deploy_docker.sh
+++ tools/deploy_docker.sh
@@ -19,9 +19,11 @@ else
   DOCKER_TAG="$TRAVIS_BRANCH"
 fi
 
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 
-docker build -f tools/Dockerfile -t "$IMAGE_NAME":"$DOCKER_TAG" .
+docker buildx build --platform linux/amd64,linux/arm64 -f tools/Dockerfile -t "$IMAGE_NAME":"$DOCKER_TAG" .
 
 echo "NEW_GIT_TAG=$NEW_GIT_TAG"
 if [ ! -z "$NEW_GIT_TAG" ]; then
