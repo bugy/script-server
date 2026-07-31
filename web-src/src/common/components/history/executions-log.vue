627dab09add1d4486e3b86f88a570fc595cee31f
cpadlab <cpadlab@gmail.com>
2026-07-31 10:53:01 +0200

    feat: Integrate Paginator in executions log view

--- web-src/src/common/components/history/executions-log.vue
+++ web-src/src/common/components/history/executions-log.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="container">
     <PageProgress v-if="loading && !disableProgressIndicator"/>
-    <executions-log-table v-else :rowClick="goToLog" :rows="executionRows"/>
+    <div v-else class="history-log-content">
+      <executions-log-table :rowClick="goToLog" :rows="executionRows"/>
+      <Paginator
+        :page="page"
+        :pageSize="pageSize"
+        :total="total"
+        :totalPages="totalPages"
+        :pageSizeOptions="pageSizeOptions"
+        :disabled="loading"
+        @page-change="onPageChange"
+        @size-change="onSizeChange"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import {mapActions, mapState} from 'vuex';
 import PageProgress from '../PageProgress';
-import ExecutionsLogTable from './executions-log-table'
+import ExecutionsLogTable from './executions-log-table';
+import Paginator from './Paginator';
 
 export default {
   name: 'executions-log',
@@ -21,7 +34,8 @@ export default {
   },
   components: {
     'executions-log-table': ExecutionsLogTable,
-    PageProgress
+    PageProgress,
+    Paginator
   },
 
   mounted: function () {
@@ -29,29 +43,45 @@ export default {
   },
 
   methods: {
-    ...mapActions('history', ['init']),
+    ...mapActions('history', ['init', 'changePage', 'changePageSize']),
 
     goToLog(execution_entry) {
       this.$router.push({
         path: this.$router.history.current.path + '/' + execution_entry.id
       });
+    },
+
+    onPageChange(newPage) {
+      this.changePage(newPage);
+    },
+
+    onSizeChange(newSize) {
+      this.changePageSize(newSize);
     }
   },
 
   computed: {
     ...mapState('history', {
       executionRows: 'executions',
-      loading: 'loading'
+      loading: 'loading',
+      page: 'page',
+      pageSize: 'pageSize',
+      total: 'total',
+      totalPages: 'totalPages',
+      pageSizeOptions: 'pageSizeOptions'
     })
   }
 }
-
-
 </script>
 
 <style scoped>
 div.executions-log {
   height: 100%;
+}
+
+.history-log-content {
+  display: flex;
+  flex-direction: column;
 }
 
 div.progress {
